5e591e43e74599a22b7645da5a11e723e7f47911
your name goes here <thlouis0@gmail.com>
2022-04-18 22:45:41 +0200

    Erreur d'envoie au serveur

--- www/formulaire.js
+++ www/formulaire.js
@@ -12,36 +12,44 @@
 //     console.log(utilisateur.value);
 // });
 var tab=[false, false, false, false, false, false];
-const mdp1=new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}');
+const mdp1=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
 var error=document.getElementById("error");
 var mdp=document.getElementById("mdp");
 mdp.addEventListener("input", function(){
-    if(mdp1.test(mdp.value))
+    if(!mdp1.test(mdp.value))
     {
         error.textContent="Please enter a valid password";
+        tab[4]=false;
+    }else
+    {
         tab[4]=true;
+         error.textContent="";
+         
     }
 });
-    var lastname = document.getElementById("lastname");
-    lastname.addEventListener("input", function(){
-        if (lastname.value !== "")                                  
-        { 
-            tab[0]=true;
-        }
-        else{
-            error.textContent("Please enter a valid name"); }
-    });
-    var prenom=document.getElementById("prenom");
-    prenom.addEventListener("input", function(){
-    if (prenom.value !== "")                                  
+var lastname = document.getElementById("lastname");
+lastname.addEventListener("input", function(){
+    if (lastname.value !== "")                                  
     { 
+        tab[0]=true;
         error.textContent="";
-        tab[1]=true;
     }
     else{
-        error.textContent="Please enter a valid first name"; 
-    }  
-    });
+        tab[0]=false;
+        error.textContent("Please enter a valid name"); }
+});
+var prenom=document.getElementById("prenom");
+prenom.addEventListener("input", function(){
+if (prenom.value !== "")                                  
+{ 
+    error.textContent="";
+    tab[1]=true;
+}
+else{
+    tab[1]=false;
+    error.textContent="Please enter a valid first name"; 
+}  
+});
     
     
     var birthdate=document.getElementById("birthdate");
@@ -51,7 +59,8 @@ mdp.addEventListener("input", function(){
         if (!date1.test(birthdate.value))                                  
         { 
             
-            error.textContent="Please enter a valid date"; 
+            error.textContent="Please enter a valid date";
+            tab[2]=false;
         }  
         else{
            
@@ -66,10 +75,12 @@ mdp.addEventListener("input", function(){
     if(!mail1.test(mail.value))
     {
         error.textContent="Please enter a valid email adress";
+        tab[5]=false;
     }
     else{
         error.textContent="";
         tab[5]=true;
+        
     }
     });
     var username=document.getElementById("username");
@@ -77,24 +88,145 @@ mdp.addEventListener("input", function(){
         if(username.value==="")
         {
              error.textContent="Please enter a valid username";
+             tab[3]=false;
         }
         else
         {
             tab[3]=true;
+
         }
     });
+var submit=document.getElementById("form-sub");
+submit.addEventListener("submit",function(event){ 
+    event.preventDefault();
+    var verif=true;
+    for (var i = 0; i<tab.length; i++) {
+        if(tab[i] != true)
+        {
+            verif=false;
+            break;
+        }
+    }
+    if(verif == true)
+    {
+       var xhr; 
+        try {  xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
+        catch (e) 
+        {
+            try {   xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
+            catch (e2) 
+            {
+               try {  xhr = new XMLHttpRequest();  }
+               catch (e3) {  xhr = false;   }
+             }
+        }
+      
+        xhr.onreadystatechange  = function() 
+        { 
+           if(xhr.readyState  == 4)
+           {
+            if(xhr.status  == 200) 
+            {   
 
-//function verif()
-//{
-    
-    /*var name = document.forms["RegForm"]["Nom"];   
-    var prenom = document.forms["RegForm"]["prenom"];          
-    var email = document.forms["RegForm"]["Email"];    
-    var date = document.forms["RegForm"]["DateNaissance"];  
-    var username =  document.forms["RegForm"]["username"];  
-    var password = document.forms["RegForm"]["Mot de passe"]; */
-    
-   
+                //document.ajax.dyn="Received:"  + xhr.responseText; 
+                console.log(xhr.responseText); 
+              
+            }
+            else
+
+                document.ajax.dyn="Error code " + xhr.status;
+            }
+        }; 
+        //  Assigner le fichier XML à une variable
+        var data = "lastname=" + lastname.value +  "&firstname=" + prenom.value + "&birthdate="+birthdate.value+"&username="+username.value+"&userpwd="+mdp.value+"&useremail="+mail.value;
+        xhr.open("GET", "htbin/register.py",  true);
+        console.log(data);
+        xhr.send(data);
+    }else
+    {
+        error.textContent="Please enter a valid formulaire";
+    }
+});
+var tab2=[false, false];
+var error2=document.getElementById("error2");
+var usernamec=document.getElementById("username2");
+usernamec.addEventListener("input", function(){
+    if(usernamec.value==="")
+    {
+         error2.textContent="Please enter a valid username";
+         tab2[0]=false;
+    }
+    else
+    {
+        tab2[0]=true;
+
+    }
+});
+var mdp2=document.getElementById("mdp2");
+mdp2.addEventListener("input", function(){
+    if(!mdp1.test(mdp2.value))
+    {
+         error2.textContent="Please enter a valid username";
+         tab2[1]=false;
+    }
+    else
+    {
+        tab2[1]=true;
+
+    }
+});
+var submit2=document.getElementById("form-sub2");
+submit2.addEventListener("submit",function(event){ 
+    event.preventDefault();
+    var verif=true;
+    for (var i = 0; i<tab2.length; i++) {
+        if(tab2[i] != true)
+        {
+            verif=false;
+            break;
+        }
+    }
+    if(verif == true)
+    {
+       var xhr; 
+        try {  xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
+        catch (e) 
+        {
+            try {   xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
+            catch (e2) 
+            {
+               try {  xhr = new XMLHttpRequest();  }
+               catch (e3) {  xhr = false;   }
+             }
+        }
+      
+        xhr.onreadystatechange  = function() 
+        { 
+           if(xhr.readyState  == 4)
+           {
+            if(xhr.status  == 200) 
+            {   
+
+                //document.ajax.dyn="Received:"  + xhr.responseText; 
+                console.log(xhr.responseText);
+                error2.textContent=xhr.responseText; 
+              
+            }
+            else
+
+                document.ajax.dyn="Error code " + xhr.status;
+            }
+        }; 
+        //  Assigner le fichier XML à une variable
+        var data = "username="+usernamec.value+"&userpwd="+mdp2.value;
+        xhr.open("GET", "htbin/login.py",  true);
+        console.log(data);
+        xhr.send(data);
+    }else
+    {
+        error.textContent="Please enter a valid formulaire";
+    }
+}); 
     
 
 
